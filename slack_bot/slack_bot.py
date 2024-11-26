e53a1f0979c0aa3b474fb50add34082ef76ee088
Tyler Smith <145503664+Tylerrs1423@users.noreply.github.com>
2024-11-25 21:25:06 -0500

    Fix SSL certificate verification issue for Slack bot

--- slack_bot/slack_bot.py
+++ slack_bot/slack_bot.py
@@ -1,6 +1,12 @@
-import os
+import ssl
+import certifi
 from slack_sdk import WebClient
 from dotenv import load_dotenv
+import os
+
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+ssl._create_default_https_context = ssl._create_unverified_context 
 
 load_dotenv()
 slack_token = os.getenv('SLACK_API_TOKEN')
@@ -10,13 +16,13 @@ client = WebClient(token=slack_token)
 def send_message():
     try:
         response = client.chat_postMessage(
-            channel='bot-test',
+            channel='bot-test', 
             text="Hello from SLACK BOT!"
         )
         print("Message sent successfully: ", response['message']['text'])
     
     except Exception as e:
-        print("error sending message:", e)
+        print("Error sending message:", e)
 
 if __name__ == "__main__":
     send_message()
